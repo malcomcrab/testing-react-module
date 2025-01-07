368abeca3a251969e95d317f5dd4d6030af31bdf
malcomcrab <malcomcrabesq@outlook.com>
2025-01-07 14:00:23 +0000

    Finished the Dynamic section of routes

---
+++ src/components/Routes.jsx
@@ -0,0 +1,17 @@
+import App from "../App";
+import Profile from "./Profile";
+import ErrorPage from "./Error";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "profile/:name",
+    element: <Profile />,
+  },
+];
+
+export default routes;
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,15 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import Profile from "./components/Profile";
-import Spinach from "./components/Spinach";
-import Popeye from "./components/Popeye";
-import DefaultProfile from "./components/DefaultComponent";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "profile",
-    element: <Profile />,
-    children: [
-      { index: true, element: <DefaultProfile /> },
-      { path: "spinach", element: <Spinach /> },
-      { path: "popeye", element: <Popeye /> },
-    ],
-  },
-]);
+import routes from "./components/Routes";
+
+const router = createBrowserRouter(routes);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
